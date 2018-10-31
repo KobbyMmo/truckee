fe04be51bfad75f4d0ae12691c68486eeaa34b2e
Obed <obedamoasi@gmail.com>
2018-10-31 18:22:25 +0000

    init server

---
+++ README.md
@@ -0,0 +1,8 @@
+| Language | Framework | Platform | Author |
+| -------- | -------- |--------|--------|
+| Nodejs | Express | Heroku|Obed Amoasi |
+
+
+# Swingdev Node Assignment
+
+A public API to Load customer's warehouse onto trucks
---
+++ src/app.js
@@ -0,0 +1,44 @@
+
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import  'dotenv/config';
+
+const app = express();
+app.disable('x-powered-by');
+import cors from 'cors';
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(`/${process.env.API_VERSION}/static`, express.static(path.join(__dirname, '/../public')))
+
+
+
+//Base route for checking api is online
+app.use(`/${process.env.API_VERSION}/ping`, (req, res) => res.send('Api up and running'));
+
+
+//404 handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+//error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res
+        .status(err.status || 500)
+        .json({ error: err.message });
+});
+
+
+module.exports = app;
