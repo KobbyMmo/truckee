c0c2a7a601a2d2c7f7d996637ee3da3928ff175b
Obed <obedamoasi@gmail.com>
2018-10-31 21:24:09 +0000

    correct path resolution (rename entity to model)

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,autoIndex: true});
 import requireAll from 'require-all'
-requireAll(__dirname + '/../entity');
+requireAll(__dirname + '/../model');
 
 mongoose
   .connection
