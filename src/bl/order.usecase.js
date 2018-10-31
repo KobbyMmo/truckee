3213df24b78a176848c4401c13348d59b3cc34dc
Obed <obedamoasi@gmail.com>
2018-10-31 18:24:41 +0000

    add order usecase

---
+++ src/bl/order.usecase.js
@@ -0,0 +1,14 @@
+/**
+ * Contains business logic and for and Order
+ * Primary caller is {OrderController} which accesses this class to get Order History and create Order
+ *
+ * @see OrderController
+ *
+ * @author Obed Amoasi <obedamoasi@gmail.com>
+ * 
+ */
+
+class OrderUsecase {
+}
+
+export default OrderUsecase;
