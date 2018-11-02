f4c6668b813e268cc4c449af55a8011c96834e96
Obed Amoasi <obedamoasi@gmail.com>
2018-11-02 17:55:55 +0000

    move import statements up

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import path from 'path';
+import cors from 'cors';
 
 import  'dotenv/config';
 import './config/db.config';
 
 const app = express();
 app.disable('x-powered-by');
-import cors from 'cors';
 
 app.use(cors());
 app.use(logger('dev'));
